228d2fce47af35ec7e05f573d67cd849be9b5a49
UT <infoudzu@gmail.com>
2025-09-21 14:20:44 +0300

    Initial Commit

---
+++ Myfirstapp/MyfirstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyfirstappApp.swift
+//  Myfirstapp
+//
+//  Created by Алексей А on 21.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyfirstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyfirstappTests/MyfirstappTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyfirstappTests.swift
+//  MyfirstappTests
+//
+//  Created by Алексей А on 21.09.2025.
+//
+
+import Testing
+@testable import Myfirstapp
+
+struct MyfirstappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
